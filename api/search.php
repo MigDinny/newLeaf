149abee5b44c158689253d55aa506130b8442810
EdgarTip <edgartip2003@gmail.com>
2021-10-31 21:26:34 +0000

    dummy text

---
+++ api/search.php
@@ -0,0 +1,66 @@
+<?php
+   echo '
+   [
+	{
+		"id": "0001",
+		"type": "INTERNSHIP",
+		"name": "Estagio OREOS",
+		"salary": "3000",
+        "details": "É kinda cringe",
+		"company": "Marilia FC",
+		"graduation-requirements": "Licenciado",
+		"remote": "FULL",
+        "course":
+            {
+                "id": "123",
+                "name": "Engenharia Informatica",
+            }
+	},
+	{
+		"id": "0002",
+		"type": "JOB",
+		"name": "Backend Python",
+		"salary": "10000",
+        "details": "API\'s and tables go brrrrrrrrrr",
+		"company": "Gogle",
+		"graduation-requirements": "Universidade",
+		"remote": "HYBRID",
+        "course":
+            {
+                "id": "123",
+                "name": "Engenharia Informatica",
+            }
+	},
+	{
+		"id": "0003",
+		"type": "RESEARCH",
+		"name": "Bolsa Natação glup glup",
+		"salary": "10",
+        "details": "10 euros já é muito...",
+		"company": "Nautico Miranda do Corvo",
+		"graduation-requirements": "None",
+		"remote": "NO",
+        "course":
+            {
+                "id": "323",
+                "name": "Ciencias do Desporto",
+            }
+	},
+    {
+        "id": "0004",
+		"type": "RESEARCH",
+		"name": "Bolsa SpaceX",
+		"salary": "1000",
+        "details": "For studies in Mars terrain idk take my money - Elon Musk",
+		"company": "SpaceX",
+		"graduation-requirements": "Licenciatura",
+		"remote": "FULL",
+        "course":
+            {
+                "id": "333",
+                "name": "Engenharia Aeroespacial",
+            }
+    }
+]
+   ';
+?>
