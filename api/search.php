a19f65a10cb42147d86f060624e9e8a105a06677
EdgarTip <edgartip2003@gmail.com>
2021-11-06 13:24:19 +0000

    Search.php completed

--- api/search.php
+++ api/search.php
@@ -1,6 +1,18 @@
 <?php
 
+    //This script is responsible for getting hte information out of the database and echoing 
+    //it so that the fetch.js script can react according to the filters that the user put in.
+
+    //Prints out a json
+
+    //Current filters: type (obligatory), course id, salary, location, remoteness(?) and graduation requirements.
+    //More can easily be added.
+
+
+
     require_once '../settings.php';
+
+    //TEMPORARY
     //id/type/name/salary/details/company/graduation-requirements/remote/course(id/name)
     $dummy_table = array( 
         array("0001", "INTERNSHIP", "Estagio OREOS", 3000, "É kinda cringe", "Marilia FC", "Licenciado", "FULL", array("123", "Engenharia Informatica")), 
@@ -9,39 +21,48 @@
         array("0004", "RESEARCH", "Bolsa SpaceX", 1000, "For studies in Mars terrain idk take my money - Elon Musk", "SpaceX", "Licenciatura", "FULL", array("333", "Engenharia Aeroespacial"))
 
     );
+    //END TEMPORARY
+
+
+    $needs_and = false;
 
     function error(){
         echo json_encode(array());
         die();
     }
 
-    //Works!
-    //print_r($dummy_table);
-    //echo $dummy_table[1][8][1];
-    //$toJson = json_encode($dummy_table);
-    //echo $toJson;
-
     //Checks if the obligatory GET arguments exist
-    if (!isset($_GET['type']) || !isset($_GET['course_id'])){
+    if (!isset($_GET['type'])){
+ 
         error();
     } 
     
-    $course_id = $_GET['course_id'];
     $type = $_GET['type'];
     
     //Checks if the type is valid out of 3 options (RESEARCH, JOB, INTERNSHIP)
     if($type != "RESEARCH" && $type != "INTERNSHIP" && $type != "JOB"){
-        error();
-    }
 
-
-    //Checks if the course id is a number
-    if(!intval($course_id) || $course_id < 0){
         error();
     }
 
     $query = "SELECT * FROM ". $type . " WHERE";
-    
+
+    //Checks if the course id exists and is valid
+    if(isset($_GET['course_id'])){
+
+        $course_id = $_GET['course_id'];
+
+        if(!intval($course_id) || $course_id < 0){
+            error();
+        }
+
+        if($needs_and) $query .= " AND";
+
+        $query .= " course_id = " . $course_id;
+
+        $needs_and = true;
+    }
+
     //Checks to see if there is salary filter
     if(isset($_GET['salary'])){
         
@@ -52,8 +73,12 @@
             error();
         }
 
+        if($needs_and) $query .= " AND";
+        
         $query .= " salary >= " . $salary;
-        echo $query;
+        
+        $needs_and = true;
+
     }
 
     //Checks to see if there is location filter
@@ -61,7 +86,11 @@
         
         $location = $_GET['location'];
 
+        if($needs_and) $query .= " AND";
+
         $query .= " location = " . $location;
+
+        $needs_and = true;
     }
 
     //Checks to see if there is remote filter
@@ -74,23 +103,30 @@
             error();
         }
 
+        if($needs_and) $query .= " AND";
+
         $query .= " remote = " . $remote;
+
+        $needs_and = true;
     }
 
-    //Checks to see if there is graduation requirements filter
+    //Checks to see if there is graduation requirements filter and applies it
     if(isset($_GET['graduation-requirements'])){
         
         $graduation_requirements = $_GET['graduation-requirements'];
 
+        if($needs_and) $query .= " AND";
+
         $query .= " graduation_requirements = " . $graduation_requirements;
+
+        $needs_and = true;
     }
 
     
 
     echo $query;
-    #DB::query($query);
-
-
-    echo "as";
+    #$data = DB::query($query);
+    #$toJson = json_encode($data);
+    #echo toJson;
 
 ?>
