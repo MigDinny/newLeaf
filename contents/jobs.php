72c31ade685332e57037912645779d6e43567c87
João Catré <73725403+Descatres@users.noreply.github.com>
2021-11-06 22:07:42 +0000

    Filters (#6)
    * Filters
    
    Filters added to jobs, research and internships.
    
    * Filters
    
    Filters added to jobs, research and internships.
    
    * Filters
    
    Filters added to jobs, research and internships.
    (minor code change)
    
    * Filters
    
    minor code changes
    
    * apresentation update
    
    boddy remotion
    
    Co-authored-by: Miguel Dinis <mikepro2013@gmail.com>

--- contents/jobs.php
+++ contents/jobs.php
@@ -1,9 +1,45 @@
-<div class="text">Jobs</div>
+<div class="text">Trabalhos</div>
 <div id="jobs" class="page-content">
+
+<!-- Filters -->
+<!--Current filters: type (obligatory), course id, salary, location, remoteness(?) and graduation requirements.-->
+<label for="filters">Área:</label>
+<select name="filters" id="course">
+<option value="software-engineer" >Engenharia Informática</option>
+<option value="medicine">Medicina</option>
+<option value="international-relationships">Relações Internacionais</option>
+<option value="law">Direito</option>
+</select>
+
+<label for="filters">Salário:</label>
+<input name="duration" style="margin-left: 0px" type="text" placeholder="Introduzir valor" required>
+
+<label for="filters">Localização:</label>
+<input name="duration" style="margin-left: 0px" type="text" placeholder="Introduzir cidade" required>
+
+<label for="filters">Remoto:</label>
+<select name="filters" id="remote">
+<option value="full" >Completamente remoto</option>
+<option value="hybrid">Híbrido</option>
+<option value="non-remote">Não</option>
+</select>
+
+<label for="filters">Grau Escolaridade:</label>
+<select name="filters" id="grad-req">
+<option value="doctorate">Doutouramento</option>
+<option value="master">Mestrado</option>
+<option value="bashelor">Licenciatura</option>    
+<option value="highschool">10º - 12º Ano</option>
+<option value="basic-school">5º - 9º Ano</option>
+<option value="primary-school">1º - 4º ano</option>
+<option value="none">Nenhum</option>
+</select>
 
 <!-- lista com as rows das cenas -->
 
 <ul id="entry-list">
+
+
     <li class="entry-row">
         <h3>Back-end PHP + PostgreSQL</h3>
         <span><b>Localização: </b>Portugal</span><br>
