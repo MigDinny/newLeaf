e7998e8e20a70476eb0345ed0ad53e8e327a8f85
IronMan988 <rodrigoff988@gmail.com>
2021-11-10 21:57:45 +0000

    Part of the filrers done
    The filters options come from the database.
    Update to the database.

--- contents/internships.php
+++ contents/internships.php
@@ -1,3 +1,13 @@
+<?php
+$query_courses = "SELECT * FROM course;";
+$courses = DB::query($query_courses);
+
+$query_graduations= "SELECT * FROM graduation_level;";
+$graduations = DB::query($query_graduations);
+
+
+?>
+
 <div class="text">Internships</div>
 <div id="internships" class="page-content">
 
@@ -6,34 +16,38 @@
 <div class="button">
     <label for="filters">Área:</label>
     <select name="filters" id="course">
-    <option value="software-engineer" >Engenharia Informática</option>
-    <option value="medicine">Medicina</option>
-    <option value="international-relationships">Relações Internacionais</option>
-    <option value="law">Direito</option>
+    
+    <?php
+    foreach ($courses as $course) {
+        echo "<option value=" . $course['id'] . " >" . $course['name'] . "</option>";
+    }
+    ?>
+
     </select>
 
     <label for="filters">Salário:</label>
     <input name="duration" style="margin-left: 0px" type="text" placeholder="Introduzir valor" required>
 
-    <label for="filters">Localização:</label>
-    <input name="duration" style="margin-left: 0px" type="text" placeholder="Introduzir cidade" required>
+    <!--<label for="filters">Localização:</label>
+    <input name="duration" style="margin-left: 0px" type="text" placeholder="Introduzir cidade" required>-->
 
     <label for="filters">Remoto:</label>
     <select name="filters" id="remote">
     <option value="full" >Completamente remoto</option>
     <option value="hybrid">Híbrido</option>
     <option value="non-remote">Não</option>
+    <option value="all">Qualquer um</option>
     </select>
 
     <label for="filters">Grau Escolaridade:</label>
     <select name="filters" id="grad-req">
-    <option value="doctorate">Doutouramento</option>
-    <option value="master">Mestrado</option>
-    <option value="bashelor">Licenciatura</option>    
-    <option value="highschool">10º - 12º Ano</option>
-    <option value="basic-school">5º - 9º Ano</option>
-    <option value="primary-school">1º - 4º ano</option>
-    <option value="none">Nenhum</option>
+
+    <?php
+    foreach ($graduations as $graduation) {
+        echo "<option value=" . $graduation['id'] . " >" . $graduation['name'] . "</option>";
+    }
+    ?>
+    <option value="none">Qualquer um</option>
     </select>
 
     <input style="margin-left: 20px" type="submit" value="Submeter">
