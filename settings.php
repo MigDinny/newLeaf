9998bb3d7b3630282db09d0bf772d47305c218fd
Miguel Dinis <mikepro2013@gmail.com>
2021-12-10 17:14:24 +0000

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,8 +1,26 @@
 <?php
 require_once 'db.class.php';
 
+<<<<<<< Updated upstream
+=======
+$production = true;
+
+// default values
+>>>>>>> Stashed changes
 DB::$user = 'root';
 DB::$password = '';
 DB::$dbName = 'pgi';
 
+<<<<<<< Updated upstream
+=======
+// set values for production if ENV is defined
+
+if ($production == true) {
+    DB::$password = '9rwPhb8{[LoO@x68';
+    DB::$user = 'id18100393_root';
+    DB::$dbName = 'id18100393_pgi';
+}
+
+
+>>>>>>> Stashed changes
 ?>
