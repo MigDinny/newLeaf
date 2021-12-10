b9c216e0ddae48cc3382ce7f7cac549405904b8b
Miguel Dinis <mikepro2013@gmail.com>
2021-12-10 15:50:52 +0000

    database connection

--- settings.php
+++ settings.php
@@ -1,8 +1,28 @@
 <?php
 require_once 'db.class.php';
 
+error_reporting(-1);
+
+// default values
 DB::$user = 'root';
 DB::$password = '';
 DB::$dbName = 'pgi';
+
+// set values for production if ENV is defined
+if (getenv('DATABASE_URL') != false) {
+
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+
+    DB::$user = $dbopts["user"];
+    DB::$password = $dbopts["pass"];
+    DB::$dbName = ltrim($dbopts["path"],'/');
+    DB::$host = $dbopts["host"];
+    DB::$port = $dbopts["port"];
+    DB::$encoding = 'utf8';
+
+    
+
+}
+
 
 ?>
