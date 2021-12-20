8f7b1f764fd447b32111e378f6b1d2d6a0dfc1ca
Miguel Dinis <mikepro2013@gmail.com>
2021-12-20 22:09:37 +0000

    Update db4.sql

--- dbs/db4.sql
+++ dbs/db4.sql
@@ -98,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `graduation_level` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `graduation_level`
