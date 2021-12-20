15ab4453d88a0efa13906c24a736823152cc4691
Miguel Dinis <mikepro2013@gmail.com>
2021-12-20 20:21:57 +0000

    done

--- contents/home.php
+++ contents/home.php
@@ -1,14 +1,72 @@
+<?php
+
+$course_list = DB::query("SELECT * FROM course;");
+
+?>
+
 <div id="homepage-centered">
     <h1 style="font-weight: 300; font-size: 40px;">EstagIO</h1>
     <br>
     
     <div style="width: 25%; min-width: 250px;">
-        <select id="select-search-course">
-            <option value="" disabled selected hidden>Escolhe o teu curso</option>
-            <option value="1">Engenharia Informática</option>
-            <option value="2">Medicina</option>
+        <select id="select-search-course" onchange="selectCourse(this);">
+            <?php
+                $alreadySelected = false;
+
+                if (!isset($_COOKIE['selected_course'])) {
+                    echo "<option value='' disabled selected hidden>Escolhe um curso</option>";
+                    $alreadySelected = true;
+                }
+
+                foreach ($course_list as $course) {
+                    if ($alreadySelected == false && $course['id'] == $_COOKIE['selected_course']) {
+                        echo "<option value='" . $course['id']  . "' selected>" . $course['name'] . "</option>";
+                        $alreadySelected = true;
+                    } else echo "<option value='" . $course['id']  . "'>" . $course['name'] . "</option>";
+                }
+                
+                if ($alreadySelected == false) echo "<option value='' disabled selected hidden>Escolhe um curso</option>";
+            ?>
         </select>
+
+
+       
     </div>
+    <br>
+    <div class="card-row">
+            <div class="card-column">
+                
+                <a href="?section=jobs">
+                    <div class="card">
+                        <h3>Empregos</h3>
+                        <p>93 Propostas</p>
+                        <p>Some text</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card-column">
+
+                <a href="?section=internships">
+                    <div class="card">
+                        <h3>Estágios</h3>
+                        <p>159 Propostas</p>
+                        <p>Some text</p>
+                    </div>
+                </a>
+            </div>
+            
+            <div class="card-column">
+                <a href="?section=research">
+                    <div class="card">
+                        <h3>Investigação</h3>
+                        <p>65 Propostas</p>
+                        <p>Some text</p>
+                    </div>
+                </a>
+            </div>
+            
+        </div>
 </div>
 
 <script>
--- index.php
+++ index.php
@@ -13,7 +13,7 @@ require_once 'settings.php';
 	<!-- Sidebar styles -->
 	<link rel="stylesheet" href="static/sidebar.css" />
   
-  
+  <script src="https://cdn.jsdelivr.net/npm/js-cookie/src/js.cookie.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.min.js" integrity="sha256-+C0A5Ilqmu4QcSPxrlGpaZxJ04VjsRjKu+G82kl5UJk=" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/css/selectize.bootstrap3.min.css" integrity="sha256-ze/OEYGcFbPRmvCnrSeKbRTtjG4vGLHXgOqsyLFTRjg=" crossorigin="anonymous" />
@@ -152,6 +152,7 @@ else {
 </section>
 
 <script type="text/javascript" src="static/sidebar.js"></script>
+<script type="text/javascript" src="static/home.js"></script>
 
 </body>
 </html>
