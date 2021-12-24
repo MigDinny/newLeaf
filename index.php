93a0aeb5dfa35dff9921b07a39d6ba497cafe764
Miguel Dinis <mikepro2013@gmail.com>
2021-12-24 14:58:24 +0000

    deploy

--- index.php
+++ index.php
@@ -1,136 +1,230 @@
 <?php
+require_once 'settings.php';
 
-require_once "settings.php";
+// check course cookie and redirect 
+if (isset($_GET['section'])) {
+    if ($_GET['section'] == 'jobs' || $_GET['section'] == 'internships' || $_GET['section'] == 'research') {
 
-$show = false;
-$email = null;
-if (isset($_POST['email'])) {
-	$email = $_POST['email'];
-	
-	DB::insert('emails', [
-		'email' => $email,
-		'ip' => $_SERVER['REMOTE_ADDR'],
-		'ua' => $_SERVER['HTTP_USER_AGENT']
-	]);
-	
-	$show = true;
+        if (!isset($_COOKIE['selected_course'])) header("Location: /?msg=not_selected");
+
+    }
 }
 
 ?>
+
 <html>
-
 <head>
+
 	<title>newLeaf</title>
-	<link rel="icon" href="static\images\logo-min.png">
+	<link rel="stylesheet" type="text/css" href="static/style.css" />
+  <link rel="icon" href="static\images\logo-min.png">
 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<style type="text/css" rel="stylesheet">
-		@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+	<!-- Sidebar styles -->
+	<link rel="stylesheet" href="static/sidebar.css" />
 
-		body {
-			background-image: url('static/images/desktop.jpg');
-			background-size: cover;
-		}
+  <script src="https://cdn.jsdelivr.net/npm/js-cookie/src/js.cookie.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.min.js" integrity="sha256-+C0A5Ilqmu4QcSPxrlGpaZxJ04VjsRjKu+G82kl5UJk=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/css/selectize.bootstrap3.min.css" integrity="sha256-ze/OEYGcFbPRmvCnrSeKbRTtjG4vGLHXgOqsyLFTRjg=" crossorigin="anonymous" />
 
-		
-		#content {
-			width: 99%;
-			font-family: 'Poppins';
-			text-align: center;
-			position: absolute;
-			top: 50%;
-			transform: translateY(-50%);
-			
-		}
-		
-		form .button input{
-			height: 45px;
-			margin: 35px 0
-		}
-		
-		form .button {
-   
-   
-			border-radius: 5px;
-			border: none;
-			color: #fff;
-			font-size: 18px;
-			font-weight: 500;
-			letter-spacing: 1px;
-			cursor: pointer;
-			transition: all 0.3s ease;
-			background: linear-gradient(135deg, #11101d, #391058);
-		}
 
-		form .button:hover{
-			/* transform: scale(0.99); */
-			background: linear-gradient(-135deg, #505050, #505051);
-		}
-		
-		input{
-			font-family: 'Poppins';
-			width: 30%;
-			text-align: center;
-			outline: none;
-			font-size: 16px;
-			border-radius: 5px;
-			padding: 15px;
-			border: 1px solid #ccc;
-			border-bottom-width: 2px;
-			transition: all 0.3s ease;
-		}
-		input:focus, input:valid{
-			border-color: #505050;
-		}
-		
-		#img {
-			width: 25%;
-		}
-		
-		
-		@media only screen and (orientation: portrait) {
-			body {
-				background-size: cover;
-				background-image: url('static/images/mobile.jpg');
-			}
-			
-			#img { width: 75%; margin-bottom: 150px; }
-			
-			input { width: 75%; height: 150px; font-size: 30px; border-radius: 18px; }
-								
-			#msg { font-size: 80px; margin-bottom: 150px; }
-			
-			.button { font-size: 30px; margin-top: 30px; border-radius: 18px; }
-			
-		}
-		
-		@media(max-width: 584px){
-			.container{
-			max-width: 100%;
-			}
-		}
-		
-		
-	</style>
+  <!-- Boxicons CDN Link -->
+  <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet' />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+
+  <!-- Offer creation Link -->
+
+
+  <?php
+
+  if (isset($_GET['section'])) {
+    switch ($_GET['section']) {
+
+      case 'home':
+        
+        break;
+      
+      case 'create-offer':
+        echo '<link rel="stylesheet" href="static/offer.css">';
+        break;
+      
+      case 'about':
+        echo '<link rel="stylesheet" href="static/about_us.css">';
+        break;
+
+    }
+  } else {
+    echo '<script src="static/home.js"></script>';
+  }
+
+  ?>
+
+
 </head>
+
 <body>
-<div id="content">
-<img id="img" src="static/images/logo.png" />
-<h1 style="color: white;" id="msg">COMING SOON...</h1>
 
-<?php 
+<!-- SIDEBAR (common) -->
+  <div class="sidebar">
+      <!--<div class="logo">
+        <div class="logo-details">
+          <img src="static/images/azul2.png" alt="logoImg">
+          <div class="name_job">
+            <div class="name"></div>
+          </div>
+        </div>
 
-	if ($show) echo "<h1 style='color: green; font-size: 20px;'>Thanks for subscribing!</h2>";
+         
+      </div>-->
+
+    <div class="logo-details">
+        <div class="logo_name">
+        <!-- <a href="/index3.php" style = "color: #fff"> -->
+        <a href="/" style = "color: #fff">
+        <img src="static/images/logo-min.png" alt="logoImg" style="height: 50px;vertical-align: middle;margin-right: 10px;"/>
+        <span> newLeaf  </span>
+        </a>
+
+        </div>
+        <i class='bx bx-menu' id="btn" ></i>
+    </div>
+    <ul class="nav-list">
+
+
+      <?php
+      if (!isset($_GET['section'])) {}
+      else if ($_GET['section'] == 'jobs' || $_GET['section'] == 'internships' || $_GET['section'] == 'research') {
+        echo '<li>
+            <i class=\'bx bx-search\' ></i>
+            <input type="text" id="search-bar" placeholder="Search..." onChange="fetchData(\''.$_GET['section'].'\');">
+            <span class="tooltip">Search</span>
+            </li>';
+      }
+      ?>
+
+
+      <?php
+
+        if (isset($_COOKIE['selected_course'])) {
+            echo '<li>
+            <a href="?section=jobs">
+              <i class=\'bx bx-briefcase\'></i>
+              <span class="links_name">Empregos</span>
+            </a>
+             <span class="tooltip">Empregos</span>
+          </li>
+          <li>
+           <a href="?section=internships">
+             <i class=\'bx bxs-graduation\' ></i>
+             <span class="links_name">Estágios</span>
+           </a>
+           <span class="tooltip">Estágios</span>
+         </li>      
+          <li>
+           <a href="?section=research">
+             <i class=\'bx bx-archive\' ></i>
+             <span class="links_name">Bolsas de investigação</span>
+           </a>
+           <span class="tooltip">Bolsas de investigação</span>
+         </li>';
+        }
+
+      ?>
+
+
+     <li>
+       <a href="?section=about">
+         <i class='bx bx-info-circle' ></i>
+         <span class="links_name">Sobre nós</span>
+       </a>
+       <span class="tooltip">Sobre nós</span>
+     </li>
+     <!-- 
+     <li>
+       <a href="#">
+         <i class='bx bx-pie-chart-alt-2' ></i>
+         <span class="links_name">Analytics</span>
+       </a>
+       <span class="tooltip">Analytics</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-folder' ></i>
+         <span class="links_name">File Manager</span>
+       </a>
+       <span class="tooltip">Files</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-heart' ></i>
+         <span class="links_name">Saved</span>
+       </a>
+       <span class="tooltip">Saved</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-cog' ></i>
+         <span class="links_name">Setting</span>
+       </a>
+       <span class="tooltip">Setting</span>
+     </li>
+    
+     <li class="profile">
+         <div class="profile-details">
+           <img src="static/images/user-profile.jpg" alt="profileImg">
+           <div class="name_job">
+             <div class="name">Username</div>
+             <div class="job">User's Job</div>
+           </div>
+         </div>
+         <i class='bx bx-log-out' id="log_out" ></i>
+     </li>
+    -->
+    </ul>
+    
+</div>
+
+
+<!-- CONTENTS (depends on section) -->
+
+<section class="home-section">
+<?php
+
+if (!isset($_GET['section'])) include "contents/home.php";
+else {
+	switch($_GET['section']) {
+    case 'create_offer':
+      include "contents/create_offer.php";
+      break;
+		
+		case 'research':
+			include "contents/research.php";
+			break;
+		
+    case 'internships':
+      include "contents/internships.php";
+      break;
+
+    case 'jobs':
+      include "contents/jobs.php";
+      break;
+
+    case 'about':
+      include "contents/about_us.php";
+      break;
+      
+		default:
+			include "contents/home.php";
+			break;
+	}
+}
 
 ?>
+</section>
 
-<form action="" method="POST">
-<input type="email" name="email" placeholder="Insert your email to be notified on release" />
-<br><br>
-<input class="button" type="submit" value="Submit" />
-</form>
-
-</div>
+<script type="text/javascript" src="static/sidebar.js"></script>
 
 </body>
 </html>
